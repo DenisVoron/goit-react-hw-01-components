de9594644204a893ab9984671b5ab3411c910185
DenisVoron <Denis.voronko@gmail.com>
2022-09-26 23:25:41 +0300

    make task2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,25 @@
-import {Profile} from 'components/Profile/Profile';
+//import PropTypes from 'prop-types';
+
+import { Profile } from 'components/Profile/Profile';
+import user from '../user.json';
+
+import { Statistics } from 'components/Statistics/Statistics';
+import data from '../data.json';
 
 export const App = () => {
   return (
-    <Profile />
+    <>
+      <Profile
+        userName={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        statsFollowers={user.stats.followers}
+        statsViews={user.stats.views}
+        statsLikes={user.stats.likes}
+      />
+      <Statistics title="Upload stats" stats={data}/>
+    </>
   );
 };
+
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,34 +1,54 @@
-import css from './Profile.module.css';
-console.log(css);
+import PropTypes from 'prop-types';
 
-export const Profile = ({username,tag,location,avatar }) => {
+import css from './Profile.module.css';
+
+export const Profile = ({
+  userName,
+  tag,
+  location,
+  avatar,
+  statsFollowers,
+  statsViews,
+  statsLikes
+}) => {
+
     return (
        <div className={css.profile}>
-         <div className="description">
+         <div className={css.description}>
            <img
-             src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-             alt="User avatar"
-             className="avatar"
+             src={avatar}
+             alt={userName}
+             className={css.avatar}
            />
-           <p className="name">Petra Marica</p>
-           <p className="tag">@pmarica</p>
-           <p className="location">Salvador, Brasil</p>
+          <p className={css.name}>{userName}</p>
+          <p className={css.tag}>{tag}</p>
+          <p className={css.location}>{location}</p>
          </div>
 
-         <ul className="stats">       
+         <ul className={css.stats}>       
            <li>
-            <span className="label">Followers</span>
-            <span className="quantity">1000</span>
+            <span className={css.label}>Followers</span>
+            <span className={css.quantity}> {statsFollowers}</span>
            </li>
            <li>
-             <span className="label">Views</span>
-             <span className="quantity">2000</span>
+             <span className={css.label}>Views</span>
+            <span className={css.quantity}> {statsViews}</span>
            </li>
            <li>
-             <span className="label">Likes</span>
-             <span className="quantity">3000</span>
+             <span className={css.label}>Likes</span>
+            <span className={css.quantity}> {statsLikes}</span>
            </li>
          </ul>
        </div>
     );
-}       
+}
+
+Profile.propTypes = {
+  userName: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  statsFollowers: PropTypes.number.isRequired,
+  statsViews: PropTypes.number.isRequired,
+  statsLikes: PropTypes.number.isRequired,
+};
